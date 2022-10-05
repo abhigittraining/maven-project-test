28bc1ea29116cbf2fc0407333b0aa208aeb08cc4
abhigittraining <potekarabhishek27@gmail.com>
2022-10-05 19:17:45 +0530

    adding to github

---
+++ maven-project-test/src/main/java/test/maven_project_test/App.java
@@ -0,0 +1,13 @@
+package test.maven_project_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
